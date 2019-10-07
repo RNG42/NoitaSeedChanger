fa034da819df84593b5d92f168abda9f314da6c7
RNG42 <21169392+RNG42@users.noreply.github.com>
2019-10-07 09:02:42 +0200

    Update Imports.cs
    Added imports for ini file editing

--- NoitaSeedChanger/Imports.cs
+++ NoitaSeedChanger/Imports.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace NoitaSeedChanger
 {
@@ -13,5 +14,11 @@ namespace NoitaSeedChanger
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        public static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        public static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
     }
 }
