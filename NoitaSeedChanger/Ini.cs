43abbd0c5e82fb6ab597b91ee63f1214110f82e6
RNG42 <21169392+RNG42@users.noreply.github.com>
2019-10-14 12:50:14 +0200

    Update Ini.cs

--- NoitaSeedChanger/Ini.cs
+++ NoitaSeedChanger/Ini.cs
@@ -13,34 +13,28 @@ internal class Ini
 
     [DllImport("kernel32", CharSet = CharSet.Unicode)]
     public static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
-
     public Ini(string IniPath = null)
     {
         Path = new FileInfo(IniPath ?? (EXE + ".ini")).FullName.ToString();
     }
-
     public string Read(string Key, string Section = null)
     {
         StringBuilder RetVal = new StringBuilder(255);
         GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
         return RetVal.ToString();
     }
-
     public void Write(string Key, string Value, string Section = null)
     {
         WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
     }
-
     public void DeleteKey(string Key, string Section = null)
     {
         Write(Key, null, Section ?? EXE);
     }
-
     public void DeleteSection(string Section = null)
     {
         Write(null, null, Section ?? EXE);
     }
-
     public bool KeyExists(string Key, string Section = null)
     {
         return Read(Key, Section).Length > 0;
