da3fc19def72023e00d1b15e6ac73441c278b0d0
RNG42 <21169392+RNG42@users.noreply.github.com>
2019-10-14 12:47:34 +0200

    Update Program.cs

--- NoitaSeedChanger/Program.cs
+++ NoitaSeedChanger/Program.cs
@@ -12,20 +12,21 @@ namespace NoitaSeedChanger
         private static readonly string settingsFile = AppDomain.CurrentDomain.BaseDirectory + "settings.ini";
         private static readonly string listFile = AppDomain.CurrentDomain.BaseDirectory + "seeds.txt";
 
-        private static int mode = 0;
-        public static uint seed = 0;        // 1 to 4294967295
+        private static int version = 0;
+        public static uint seed = 0;        // 0 to 4294967294 +1
         private static bool restart = false;
 
         private static readonly int[] pointer = new int[] { 
             0x14136D4, 0x1420798, 0x14ABCF0, // release branch
             0x1420798, 0x14ABCF0, 0x1420788, // beta branch
-            0x1429880, 0x14B4DE0, 0x14296C0, // mod branch
-            0x177712C, 0x1801640, 0x1777AC8  // old version
+            0x142B898, 0x14B6E20, 0x142B6C8, // mod branch
+            0x177712C, 0x1801640, 0x1777AC8, // old version
+            0x1423ACC, 0x14AF030, 0x1421208  // GoG version
         };
 
         static void Main(string[] args)
         {
-            // hooked CancelKeyPress to RestartApp function
+            // hooked Restart function to CancelKeyPress event
             Console.CancelKeyPress += new ConsoleCancelEventHandler(RestartApp);
 
             if (!File.Exists(settingsFile)) // check if settings.ini exists
@@ -33,13 +34,13 @@ namespace NoitaSeedChanger
                 File.Create(settingsFile).Close();
 
                 settings = new Ini(settingsFile);
-                settings.Write("beta", "false", "Settings");
-                mode = Convert.ToInt32(settings.Read("mode", "Settings"));
+                settings.Write("version", "0", "Settings");
+                version = Convert.ToInt32(settings.Read("version", "Settings"));
             }
             else // Load settings
             {
                 settings = new Ini(settingsFile);
-                mode = Convert.ToInt32(settings.Read("mode", "Settings"));
+                version = Convert.ToInt32(settings.Read("version", "Settings"));
             }
 
             if (!File.Exists(listFile)) // check if seedlist.txt exists
@@ -49,7 +50,7 @@ namespace NoitaSeedChanger
                 File.WriteAllLines(listFile, lines);
             }
 
-            Banner.Draw();
+            Helper.DrawBanner();
 
             // get seed from list
             if (!Seedlist.GetList(listFile))
@@ -59,18 +60,20 @@ namespace NoitaSeedChanger
                 Console.ForegroundColor = ConsoleColor.White;
                 uint.TryParse(Console.ReadLine(), out seed);
                 Console.Write(Environment.NewLine);
+            }
 
-                if (seed <= 0)  // game stucks on title screen if the seed is less or equal zero
-                {
-                    seed = 1;
-                }
+            if (seed <= 0)  // game stucks on title screen if the seed is less or equal zero
+            {
+                seed = Helper.RandomSeed();
+                Helper.WriteLine("New randomly generated seed is " + seed);
+                Console.Write(Environment.NewLine);
             }
 
             Restart:
 
             if (restart)
             {
-                Banner.Draw();
+                Helper.DrawBanner();
                 Console.ForegroundColor = ConsoleColor.White;
             }
 
@@ -80,7 +83,7 @@ namespace NoitaSeedChanger
             // checks if noita.exe is running
             while (game == null)
             {
-                System.Threading.Thread.Sleep(25);
+                System.Threading.Thread.Sleep(50);
                 if (Process.GetProcessesByName(gameName).Length > 0)
                 {
                     game = Process.GetProcessesByName(gameName)[0];
@@ -92,7 +95,7 @@ namespace NoitaSeedChanger
             // writes seed to given memory address for the correct version
             if (game.WaitForInputIdle())
             {
-                switch (mode)
+                switch (version)
                 {
                     case 0: // release
                         ChangeSeed(pointer[0], pointer[1], pointer[2]);
@@ -105,6 +108,9 @@ namespace NoitaSeedChanger
                         break;
                     case 3: // old
                         ChangeSeed(pointer[9], pointer[10], pointer[11]);
+                        break;
+                    case 4: // GoG
+                        ChangeSeed(pointer[12], pointer[13], pointer[14]);
                         break;
                     default:
                         break;
@@ -128,15 +134,15 @@ namespace NoitaSeedChanger
             goto Restart;
         }
 
-        private static void ChangeSeed(int addr1, int addr2, int addr3)
+        private static void ChangeSeed(int p1, int p2, int p3)
         {
-            while (Memory.Read(game.Handle, addr1) != seed && Memory.Read(game.Handle, addr2) != seed)
+            while (Memory.Read(game.Handle, p1) != seed && Memory.Read(game.Handle, p2) != seed)
             {
-                if (Memory.Read(game.Handle, addr1) > 0 || Memory.Read(game.Handle, addr2) > 0)
+                if (Memory.Read(game.Handle, p1) > 0 || Memory.Read(game.Handle, p2) > 0)
                 {
-                    Memory.Write(game.Handle, addr1, seed);
-                    Memory.Write(game.Handle, addr2, seed);
-                    Memory.Write(game.Handle, addr3, seed);
+                    Memory.Write(game.Handle, p1, seed);
+                    Memory.Write(game.Handle, p2, seed);
+                    Memory.Write(game.Handle, p3, seed);
                 }
             }
         }
