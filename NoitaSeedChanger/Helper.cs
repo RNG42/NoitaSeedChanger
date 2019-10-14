c19d69f22e236c2a3b016a90f6eb733f09e0b0d3
RNG42 <21169392+RNG42@users.noreply.github.com>
2019-10-14 12:45:45 +0200

    Create Helper.cs

---
+++ NoitaSeedChanger/Helper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace NoitaSeedChanger
+{
+    class Helper
+    {
+        public static void DrawBanner()
+        {
+            ConsoleColor[] gradient = new ConsoleColor[5]
+            {
+            ConsoleColor.Cyan,
+            ConsoleColor.DarkCyan,
+            ConsoleColor.Blue,
+            ConsoleColor.DarkBlue,
+            ConsoleColor.DarkBlue
+            };
+            string[] banner = new string[5]
+            {
+            "     _______         __ __              _______                 __      ______ __                                 ",
+            "    |    |  |.-----.|__|  |_.---.-.    |     __|.-----.-----.--|  |    |      |  |--.---.-.-----.-----.-----.----.",
+            "    |       ||  _  ||  |   _|  _  |    |__     ||  -__|  -__|  _  |    |   ---|     |  _  |     |  _  |  -__|   _|",
+            "    |__|____||_____||__|____|___._|    |_______||_____|_____|_____|    |______|__|__|___._|__|__|___  |_____|__|  ",
+            "                                                                                                |_____|           "
+            };
+            for (int i = 0; i < gradient.Length; i++)
+            {
+                Console.ForegroundColor = gradient[i];
+                Console.WriteLine(banner[i]);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(Environment.NewLine);
+            Console.Write(Environment.NewLine);
+        }
+
+        public static string StringSpaces(string str, int length)
+        {
+            if (str.Length < length)
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(str);
+                for (int i = 0; i < length - str.Length; i++)
+                {
+                    builder.Append(" ");
+                }
+                str = builder.ToString();
+            }
+            return str;
+        }
+
+        public static void WriteLine(string text)
+        {
+            if (text.Any(char.IsDigit))
+            {
+                string[] snippets = text.Split(char.Parse(" "));
+
+                for (int i = 0; i < snippets.Length; i++)
+                {
+                    if (snippets[i].Any(char.IsDigit))
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(snippets[i] + " ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.Write(snippets[i] + " ");
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write(Environment.NewLine);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine(text);
+            }
+        }
+
+        public static uint RandomSeed()
+        {
+            Random r = new Random();
+            int r1 = r.Next(0, int.MaxValue);
+            int r2 = r.Next(0, int.MaxValue);
+            return (uint)(r1 + r2) + 1;
+        }
+    }
+}
