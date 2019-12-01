f6c5a8a3d952a0aa923fe50e42491ece003b9f92
RNG42 <21169392+RNG42@users.noreply.github.com>
2019-12-01 10:01:14 +0100

    Update Release.cs

--- NoitaSeedChanger/Release.cs
+++ NoitaSeedChanger/Release.cs
@@ -1,39 +1,77 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Xml;
 
 namespace NoitaSeedChanger
 {
     class Release
     {
-        private static readonly string hashFile = "/_version_hash.txt";
+        private static Dictionary<string, List<string>> data = new Dictionary<string, List<string>>();
 
-        // release version hashes
-        private const string final = "b71aff760755c178c9f09ebd1e61ddc5272ceb4b";
-        private const string beta = "e756391245ea6a68b1d6e39e732bf12cd459c1e9";
+        public static string currentHash = string.Empty;
+        public static List<IntPtr> currentTargets = new List<IntPtr>();
 
-        public static void Set()
+        public static void Init()
+        {
+            GetCurrentVersionHash();
+            ReadXML("VersionData.xml");
+            GetTargetList(currentHash);
+        }
+
+        private static void ReadXML(string fileName)   // Reads XML and fills data dictionary
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileName);
+
+            XmlNodeList Nodes = doc.DocumentElement.SelectNodes("/Version/Data");
+
+            foreach (XmlNode node in Nodes)
+            {
+                string hash = "";
+                List<string> targets = new List<string>();
+
+                for (int i = 0; i < node.ChildNodes.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        hash = node.ChildNodes[i].InnerText;
+                    }
+                    else
+                    {
+                        targets.Add(node.ChildNodes[i].InnerText);
+                    }
+                }
+                data.Add(hash, targets);
+            }
+        }
+
+        public static void GetTargetList(string hash)
+        {
+            List<IntPtr> targetList = new List<IntPtr>();
+
+            foreach (var item in data)
+            {
+                if (item.Key == currentHash)
+                {
+                    foreach (var target in item.Value)
+                    {
+                        targetList.Add((IntPtr)int.Parse(target, NumberStyles.AllowHexSpecifier));
+                    }
+                }
+            }
+            currentTargets = targetList;
+        }
+
+        public static void GetCurrentVersionHash()
         {
             try
             {
-                string[] lines = File.ReadAllLines(GetHashFile(), Encoding.UTF8);
-                switch (lines[0])
-                {
-                    case final:
-                        Program.release = 0;
-                        break;
-                    case beta:
-                        Program.release = 1;
-                        break;
-                    default:
-                        Helper.Error("Current game version not supported!");
-                        Helper.Error("Closing NSC in 10 seconds.");
-                        Thread.Sleep(10000);
-                        Process.GetCurrentProcess().Kill();
-                        break;
-                }
+                string hash = File.ReadAllLines(GetHashFile(), Encoding.UTF8)[0];
+                currentHash = hash;
             }
             catch (Exception e)
             {
@@ -44,10 +82,10 @@ namespace NoitaSeedChanger
             }
 
         }
+
         private static string GetHashFile()
         {
-            return Path.GetDirectoryName(Program.game.MainModule.FileName) + hashFile;
+            return Path.GetDirectoryName(Program.game.MainModule.FileName) + "/_version_hash.txt";
         }
-
     }
 }
