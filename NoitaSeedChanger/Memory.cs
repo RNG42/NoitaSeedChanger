b122235ab8a68930c02efb2e8a865493662c72a5
RNG42 <21169392+RNG42@users.noreply.github.com>
2019-12-01 10:00:55 +0100

    Update Memory.cs

--- NoitaSeedChanger/Memory.cs
+++ NoitaSeedChanger/Memory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 internal class Memory
@@ -8,29 +9,29 @@ internal class Memory
     [DllImport("kernel32", SetLastError = true)]
     public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
 
-    public static uint Read(IntPtr handle, int address)
+    public static uint Read(IntPtr handle, IntPtr address)
     {
         byte[] dataBuffer = new byte[4];
         int bytesRead = 0;
-        ReadProcessMemory((int)handle, address, dataBuffer, dataBuffer.Length, ref bytesRead);
+        ReadProcessMemory((int)handle, (int)address, dataBuffer, dataBuffer.Length, ref bytesRead);
         return BitConverter.ToUInt32(dataBuffer, 0);
     }
 
-    public static bool Write(IntPtr handle, int address, uint value)
+    public static bool Write(IntPtr handle, IntPtr address, uint value)
     {
         byte[] dataBuffer = BitConverter.GetBytes(value);
         int bytesWritten = 0;
-        WriteProcessMemory((int)handle, address, dataBuffer, dataBuffer.Length, ref bytesWritten);
+        WriteProcessMemory((int)handle, (int)address, dataBuffer, dataBuffer.Length, ref bytesWritten);
         return true;
     }
 
-    public static void ChangeSeed(IntPtr game, uint seed, params int[] pointer)
+    public static void ChangeSeed(IntPtr game, uint seed, List<IntPtr> pointer)
     {
         while (Read(game, pointer[0]) != seed && Read(game, pointer[1]) != seed)
         {
             if (Read(game, pointer[0]) > 0 || Read(game, pointer[1]) > 0)
             {
-                for (int i = 0; i < pointer.Length; i++)
+                for (int i = 0; i < pointer.Count; i++)
                 {
                     Write(game, pointer[i], seed);
                 }
